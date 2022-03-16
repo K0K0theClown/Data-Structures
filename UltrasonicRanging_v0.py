10e1a9de442fcdbc6b2e47adf9e010a18962a6b0
K0K0theClown <77641234+K0K0theClown@users.noreply.github.com>
2022-03-16 14:58:05 +0000

    Add files via upload

--- UltrasonicRanging_v0.py
+++ UltrasonicRanging_v0.py
@@ -7,6 +7,7 @@
 ########################################################################
 import time
 import io
+import statistics
 
 def is_raspberrypi():
     try:
@@ -15,12 +16,52 @@ def is_raspberrypi():
     except Exception: pass
     return False
 
+def Hold():
+    if is_raspberrypi() == 6 <= is_raspberrypi() <= 16:
+        list = []
+        average = sum(list) / len(list)
+        stddev = statistics.stdev(list)
+        if (stddev<0.1*average):
+            if average < 12:
+                return 'Low Hold'
+            elif average > 30 and average < 60:
+                return 'High Hold'
+        return 'Unknown'
+
+def Pass():
+    if is_raspberrypi() == 26 <= is_raspberrypi() <= 36:
+        list = []
+        for low in range(len(list)):
+            if list[low] < 150:
+                break
+        for high in range(len(list)-1,low,-1):
+            if list[high] < 150:
+                break
+        if low > 0.2*len(list()) and high < 0.8*len(list):
+            result = list()
+            if result == 'Low Hold':
+                return 'Low Pass'
+            elif result == 'High Hold':
+                return 'High Pass'
+    return 'Unknown'
+
+def Gesture():
+    val = Hold()
+    if val != 'Unknown':
+        return val
+    val = Pass()
+    if val != 'Unknown':
+        return val
 
 def loop():
     while(True):
         distance = device.getSonar() # get distance
-        print ("The distance is : %.2f cm"%(distance))
-        time.sleep(0.1)
+        if distance > 100:
+            print("OUT OF RANGE")
+        else:
+            print ("The distance is : %.2f cm"%(distance))
+            print("Gesture is:"+str(Gesture()))
+            time.sleep(0.5)
         
 if __name__ == '__main__':     # Program entrance
     if is_raspberrypi():
